1c16b45fed6d74bd5d6f667d5ef8573eb610e467
shioncolor <czamy00@gmail.com>
2017-06-19 17:11:11 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,17 @@
-console.log('こんにちわ世界!');
-window.alert('こんにちわ世界');
 
-console.log('こんにちわ世界');
+var number = Math.floor(Math.random()*3);
+var answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してください'));
+
+var message;
+if(answer === number){
+message='あたり';
+}else if(answer < number){
+message='残念　もっとおおきいです；；';
+}else if(answer > number){
+message='残念　もっと小さいです';
+}else{
+massage='0~3の数字を入力してください';
+}
+
+
+document.getElementById('choice').textContent=message;

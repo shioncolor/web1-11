cab54637982441557d14944f78d8ac686ad445d7
Takashi Imagire <imagire@gmail.com>
2017-06-19 20:33:38 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,20 +1,18 @@
-var answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してください'));
 var number = Math.floor(Math.random()*3);
 
 var message;
 
-while(1){
-if(answer === number){
-message='あたり';
-break;
-}else if(answer < number){
-message='残念　もっと大きいです；；';
-}else if(answer > number){
-message='残念　もっと小さいです；；';
-}else{
-massage='0~3の数字を入力してください';
-}
-answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してください'));
-}
+do{
+  var answer = parseInt(window.prompt('数あてゲーム。0～2の数字を入力してください'));
+  if(answer === number){
+    message='あたり';
+  }else if(answer < number){
+    message='残念　もっと大きいです；；';
+  }else if(answer > number){
+    message='残念　もっと小さいです；；';
+  }else{
+    massage='0~3の数字を入力してください';
+  }
+  document.getElementById('choice').textContent=message;
+}while(answer !== number);
 
-document.getElementById('choice').textContent=message;
